a3bcda19aee85255440b818d14aea9832eeed3be
Abdirahman Ahmed <Abdirahman.ahmed@kcl.ac.uk>
2022-12-09 11:18:12 +0000

    test push

--- frequencies.py
+++ frequencies.py
@@ -2,21 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
- 
   frequencies = {}
-  
+
   for item in items:
-    
+
     if isinstance(item, str):
       key = item
     else:
       key = str(item)
 
-    # Step 4
     if key in frequencies:
       frequencies[key] += 1
     else:
       frequencies[key] = 1
 
-  # Step 5
   return frequencies
